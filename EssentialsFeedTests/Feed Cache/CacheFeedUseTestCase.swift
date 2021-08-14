44d83d258f1caf7045f393f9ef3f997ee46191b4
lorenzoangelini <lore.ange@live.it>
2021-08-15 00:05:27 +0200

    save commands succed

--- EssentialsFeedTests/Feed Cache/CacheFeedUseTestCase.swift
+++ EssentialsFeedTests/Feed Cache/CacheFeedUseTestCase.swift
@@ -39,9 +39,16 @@ class FeedStore {
         
     }
     
+    
+    
     func completeInsertion(with error: NSError, at index: Int = 0){
         insertionComplition[index](error)
        
+        
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0){
+        insertionComplition[index](nil)
         
     }
     
@@ -147,6 +154,26 @@ class CacheFeedUseTestCase: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         
         XCTAssertEqual(receivedError as NSError?, insertionError)
+      
+    }
+    
+    
+    func test_save_succeedsOnSuccessfulInsertions(){
+        let items = [uniqueItem(), uniqueItem()]
+        let (sut, store)  = makeSUT()
+       
+        
+        let exp = expectation(description: "Wait for save completion")
+        var receivedError: Error?
+        sut.save(items){ error in
+            receivedError = error
+            exp.fulfill()
+        }
+        store.completeDeletionSuccessfully()
+        store.completeInsertionSuccessfully()
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertNil(receivedError)
       
     }
     
