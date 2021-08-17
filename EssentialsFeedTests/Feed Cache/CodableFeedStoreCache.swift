2b5fb4fbf8f47dad4bc39ea29ea8aaa12c78d8c8
lorenzoangelini <lore.ange@live.it>
2021-08-17 10:43:31 +0200

    Move codable conformance from the frameworks agnostic local feed

--- EssentialsFeedTests/Feed Cache/CodableFeedStoreCache.swift
+++ EssentialsFeedTests/Feed Cache/CodableFeedStoreCache.swift
@@ -12,8 +12,33 @@ class CodableFeedStore {
     
     private struct Cache: Codable {
         
-        let feed: [LocalFeedImage]
+        let feed: [CodableFeedImage]
         let timestamp: Date
+        
+        var localFeedCache: [LocalFeedImage] {
+            return feed.map {
+                $0.local
+            }
+        }
+        
+    }
+    
+    private struct CodableFeedImage: Codable {
+        private let id: UUID
+        private let description: String?
+        private let location: String?
+        private let url: URL
+        
+        init(_ image: LocalFeedImage){
+            id = image.id
+            description = image.description
+            location = image.location
+            url = image.url
+        }
+        
+        var local : LocalFeedImage {
+            return LocalFeedImage(id: id, description: description, location: location, url: url)
+        }
         
     }
     
@@ -28,7 +53,7 @@ class CodableFeedStore {
        let decoder = JSONDecoder()
        let cache = try! decoder.decode(Cache.self, from: data)
         
-        completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+        completion(.found(feed: cache.localFeedCache, timestamp: cache.timestamp))
        
         
     }
@@ -36,7 +61,8 @@ class CodableFeedStore {
     func insertItems(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionComplition){
         
         let econder = JSONEncoder()
-        let encoded = try! econder.encode(Cache(feed: feed, timestamp: timestamp))
+        let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+        let encoded = try! econder.encode(cache)
         try! encoded.write(to: storeUrl)
         
         completion(nil)
