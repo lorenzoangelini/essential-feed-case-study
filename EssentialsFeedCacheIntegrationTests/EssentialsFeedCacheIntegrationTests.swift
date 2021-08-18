1987aa4dc686a5f7b641b0ae5b9aab2565cf89b8
lorenzoangelini <lore.ange@live.it>
2021-08-18 11:56:10 +0200

    add integration test

---
+++ EssentialsFeedCacheIntegrationTests/EssentialsFeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialsFeedCacheIntegrationTests.swift
+//  EssentialsFeedCacheIntegrationTests
+//
+//  Created by Lorenzo Angelini on 18/08/21.
+//
+
+import XCTest
+
+class EssentialsFeedCacheIntegrationTests: XCTestCase {
+
+  
+
+}
